9b4ca5e10fa87663ff07a170cc59c6b1d0595382
sky <sky@skydeMacBook-Pro.local>
2016-01-26 16:37:59 +0800

    init jasongo

---
+++ README.md
@@ -0,0 +1,15 @@
+# jasongo
+# 快递到件通知系统
+##快递到件后输入用户名与手机号码，根据用户名与手机号码匹配用户成功后发送邮件给用户通知到指定地点取件，如果用户不存在则失败
+========
+#Useage
+```
+go get github.com/labstack/echo
+go get github.com/go-xorm/xorm
+git clone https://github.com/aslijiasheng/jasongo
+cd jasongo && mkdir -p server
+mkdir -p website/css
+mkdir -p website/script
+mkdir -p website/public
+```
+
---
+++ server/server.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo"
+	mw "github.com/labstack/echo/middleware"
+)
+
+type (
+	Template struct {
+		templates *template.Template
+	}
+
+	user struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	}
+)
+
+var (
+	users map[string]user
+)
+
+func (t *Template) Render(w io.Writer, name string, data interface{}) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+// Handler
+func hello(c *echo.Context) error {
+	return c.Render(http.StatusOK, "hello", "World")
+	// return c.String(http.StatusOK, "Hello, World!\n")
+}
+
+func getUsers(c *echo.Context) error {
+	return c.JSON(http.StatusOK, users)
+}
+
+func main() {
+
+	t := &Template{
+		templates: template.Must(template.ParseGlob("../website/public/hello.html")),
+	}
+
+	// Echo instance
+	e := echo.New()
+
+	// Debug mode
+	e.Debug()
+
+	// Middleware
+	e.Use(mw.Logger())
+	e.Use(mw.Recover())
+	e.Use(mw.Gzip())
+
+	e.SetRenderer(t)
+
+	// Routes
+	e.Get("/", hello)
+
+	e.Use(func(c *echo.Context) error {
+		println(c.Path()) // Prints `/users/:name`
+		return nil
+	})
+
+	e.Get("/users", getUsers)
+
+	e.Get("/users/:name", func(c *echo.Context) error {
+		// By name
+		name := c.Param("name")
+		return c.String(http.StatusOK, name)
+	})
+
+	// Start server
+	e.Run(":1323")
+}
+
+func init() {
+	users = map[string]user{
+		"1": user{
+			ID:   "1",
+			Name: "Wreck-It Ralph",
+		},
+	}
+}
